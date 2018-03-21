5621bdd486288b49ed2652fdca8c1997678b16e2
Gautier Lemaire <gautierlemaire@mbp-de-gautier.lan>
2018-03-21 15:23:42 +0100

    test programme

--- srcs/errors_handler.c
+++ srcs/errors_handler.c
@@ -1,5 +1,42 @@
 #include "fillit.h"
+void		ft_print(t_piece *piece)
+{
+	int i = 0;
+	printf("numero : %d\n");
+	while (i < 4)
+	{
+		printf("coord[%d].x = %d", i, (piece->coord[i]).x);
+		printf("coord[%d].y = %d", i, (piece->coord[i]).y);
+		i++;
+	}
+}
+int		tetriminos_error_handler(t_piece *piece)
+{
+	int i;
+	int j;
+	int is_ok;
 
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		is_ok = 0;
+		while (j < 4)
+		{
+			if (j != i && is_ok == 0)
+			{
+				if ((((piece->coord[i]).x) - ((piece->coord[j]).x)) == 1 || (((piece->coord[i]).x) - ((piece->coord[j]).x)) == -1 ||
+				(((piece->coord[i]).y) - ((piece->coord[j].y))) == 1 || (((piece->coord[i]).x) - ((piece->coord[j]).x)) == -1)
+					is_ok++;
+			}
+			j++;
+		}
+		if (is_ok == 0)
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
 int		pre_check_errors(char *buf, int ret)
 {
 	int i;
